31781465c0e622cb96cbdcb7ad7fde252ac44e03
gulistankh2011-dot <gulistankh2011@gmail.com>
2025-11-02 18:17:09 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,7 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("app-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html"]);
-    })
-  );
+self.addEventListener('install', e => {
+  console.log('Service Worker installed');
 });
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener('activate', e => {
+  console.log('Service Worker activated');
 });
+self.addEventListener('fetch', e => {});
